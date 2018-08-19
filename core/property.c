bf5d5149182b88e7daadeeec91b029d971624a5e
Michael Brown <mbrown@fensystems.co.uk>
2018-08-20 00:36:21 +0100

    core: Add basic abstraction for OCF resource properties
    Add an abstraction of OCF resource properties, along with three
    property types (boolean, integer, and string) and methods for
    converting each type to/from a string representation.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ core/property.c
@@ -0,0 +1,239 @@
+/*
+ * Copyright (C) 2018 Michael Brown <mbrown@fensystems.co.uk>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ */
+
+/** @file
+ *
+ * Resource properties
+ *
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <uniport/property.h>
+
+/*****************************************************************************
+ *
+ * Boolean properties
+ *
+ *****************************************************************************
+ */
+
+/**
+ * Format property as string
+ *
+ * @v prop		Property
+ * @v buf		String buffer
+ * @v len		Length of string buffer
+ * @v value		State variable
+ * @ret len		Length of string
+ */
+static size_t boolean_format ( struct property *prop __unused, char *buf,
+			       size_t len, const bool *value ) {
+
+	/* Format string */
+	return snprintf ( buf, len, "%s", ( *value ? "true" : "false" ) );
+}
+
+/**
+ * Parse property from a string
+ *
+ * @v prop		Property
+ * @v string		String
+ * @v value		State variable
+ * @ret rc		Return status code
+ */
+static int boolean_parse ( struct property *prop __unused, const char *string,
+			   bool *value ) {
+
+	/* Parse string */
+	if ( ( strcasecmp ( string, "true" ) == 0 ) ||
+	     ( strcmp ( string, "1" ) == 0 ) ) {
+		*value = true;
+	} else if ( ( strcasecmp ( string, "false" ) == 0 ) ||
+		    ( strcmp ( string, "0" ) == 0 ) ) {
+		*value = false;
+	} else {
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+/** Boolean property type */
+const struct property_type boolean_property =
+	PROPERTY_TYPE ( "boolean", bool, boolean_format, boolean_parse );
+
+/*****************************************************************************
+ *
+ * Integer properties
+ *
+ *****************************************************************************
+ */
+
+/**
+ * Format property as string
+ *
+ * @v prop		Property
+ * @v buf		String buffer
+ * @v len		Length of string buffer
+ * @v value		State variable
+ * @ret len		Length of string
+ */
+static size_t integer_format ( struct property *prop __unused, char *buf,
+			       size_t len, const int *value ) {
+
+	/* Format string */
+	return snprintf ( buf, len, "%d", *value );
+}
+
+/**
+ * Parse property from a string
+ *
+ * @v prop		Property
+ * @v string		String
+ * @v value		State variable
+ * @ret rc		Return status code
+ */
+static int integer_parse ( struct property *prop __unused, const char *string,
+			   int *value ) {
+	char *end;
+
+	/* Parse string */
+	*value = strtol ( string, &end, 0 );
+	if ( *end )
+		return -EINVAL;
+
+	return 0;
+}
+
+/** Integer property type */
+const struct property_type integer_property =
+	PROPERTY_TYPE ( "integer", int, integer_format, integer_parse );
+
+/*****************************************************************************
+ *
+ * String properties
+ *
+ *****************************************************************************
+ */
+
+/**
+ * Format property as string
+ *
+ * @v prop		Property
+ * @v buf		String buffer
+ * @v len		Length of string buffer
+ * @v value		State variable
+ * @ret len		Length of string
+ */
+static size_t string_format ( struct property *prop __unused, char *buf,
+			      size_t len, const char **value ) {
+
+	/* Format string */
+	return snprintf ( buf, len, "%s", *value );
+}
+
+/**
+ * Parse property from a string
+ *
+ * @v prop		Property
+ * @v string		String
+ * @v value		State variable
+ * @ret rc		Return status code
+ */
+static int string_parse ( struct property *prop __unused, const char *string,
+			  const char **value ) {
+
+	/* Parse string */
+	*value = string;
+	return 0;
+}
+
+/** String property type */
+const struct property_type string_property =
+	PROPERTY_TYPE ( "string", const char *, string_format, string_parse );
+
+/*****************************************************************************
+ *
+ * Generic interface
+ *
+ *****************************************************************************
+ */
+
+/**
+ * Format property as string
+ *
+ * @v prop		Property
+ * @v buf		String buffer
+ * @v len		Length of string buffer
+ * @v state		Resource state
+ * @ret len		Length of string
+ */
+size_t property_format ( struct property *prop, char *buf, size_t len,
+			 const void *state ) {
+
+	return prop->type->format ( prop, buf, len, ( state + prop->offset ) );
+}
+
+/**
+ * Format property as allocated string
+ *
+ * @v prop		Property
+ * @v state		Resource state
+ * @ret string		Allocated string, or NULL on allocation error
+ *
+ * The caller is responsible for freeing the allocated string.
+ */
+char * property_format_alloc ( struct property *prop, const void *state ) {
+	char *buf;
+	size_t len;
+	size_t check;
+
+	/* Calculate length */
+	len = property_format ( prop, NULL, 0, state );
+
+	/* Allocate string */
+	buf = malloc ( len + 1 /* NUL */ );
+	if ( ! buf )
+		return NULL;
+
+	/* Format string */
+	check = property_format ( prop, buf, ( len + 1 /* NUL */ ), state );
+	assert ( check == len );
+
+	return buf;
+}
+
+/**
+ * Parse property from a string
+ *
+ * @v prop		Property
+ * @v string		String
+ * @v state		Resource state
+ * @ret rc		Return status code
+ */
+int property_parse ( struct property *prop, const char *string, void *state ) {
+
+	return prop->type->parse ( prop, string, ( state + prop->offset ) );
+}
---
+++ include/uniport/property.h
@@ -0,0 +1,108 @@
+#ifndef _UNIPORT_PROPERTY_H
+#define _UNIPORT_PROPERTY_H
+
+/** @file
+ *
+ * Resource properties
+ *
+ */
+
+#include <stddef.h>
+
+/** A property */
+struct property {
+	/** Name */
+	const char *name;
+	/** Offset from start of state descriptor */
+	size_t offset;
+	/** Property type */
+	const struct property_type *type;
+};
+
+/** Define a property */
+#define PROPERTY( _name, _state, _field, _check, _type ) {		\
+	.name = _name,							\
+	.offset = ( offsetof ( _state, _field ) +			\
+		    ( ( &( ( ( _state * ) NULL )->_field ) ==		\
+			( ( _check * ) NULL ) ) ? 0 : 0 ) ),		\
+	.type = _type,							\
+	}
+
+/** A property type */
+struct property_type {
+	/** Name */
+	const char *name;
+	/** Format property as string
+	 *
+	 * @v prop		Property
+	 * @v buf		String buffer
+	 * @v len		Length of string buffer
+	 * @v value		State variable
+	 * @ret len		Length of string
+	 */
+	size_t ( * format ) ( struct property *prop, char *buf, size_t len,
+			      const void *value );
+	/** Parse property from a string
+	 *
+	 * @v prop		Property
+	 * @v string		String
+	 * @v value		State variable
+	 * @ret rc		Return status code
+	 */
+	int ( * parse ) ( struct property *prop, const char *string,
+			  void *state );
+};
+
+/** Type of a property format() method */
+#define property_format_t( _type )					\
+	size_t ( * ) ( struct property *prop, char *buf, size_t len,	\
+		       const _type *value )
+
+/** Define a property format() method */
+#define PROPERTY_FORMAT( _type, _format )				\
+	( ( property_format_t ( void ) )				\
+	  ( ( ( ( property_format_t ( _type ) ) NULL )			\
+	      == _format ) ? _format : _format ) )
+
+/** Type of a property parse() method */
+#define property_parse_t( _type )					\
+	int ( * ) ( struct property *prop, const char *string,		\
+		    _type *value )
+
+/** Define a property parse() method */
+#define PROPERTY_PARSE( _type, _parse )					\
+	( ( property_parse_t ( void ) )					\
+	  ( ( ( ( property_parse_t ( _type ) ) NULL )			\
+	      == _parse ) ? _parse : _parse ) )
+
+/** Define a property type */
+#define PROPERTY_TYPE( _name, _type, _format, _parse ) {		\
+	.name = _name,							\
+	.format = PROPERTY_FORMAT ( _type, _format ),			\
+	.parse = PROPERTY_PARSE ( _type, _parse ),			\
+	}
+
+extern const struct property_type boolean_property;
+extern const struct property_type integer_property;
+extern const struct property_type string_property;
+
+/** Define a boolean property */
+#define PROPERTY_BOOLEAN( _name, _state, _field ) \
+	PROPERTY ( _name, _state, _field, bool, &boolean_property )
+
+/** Define an integer property */
+#define PROPERTY_INTEGER( _name, _state, _field ) \
+	PROPERTY ( _name, _state, _field, int, &integer_property )
+
+/** Define a string property */
+#define PROPERTY_STRING( _name, _state, _field ) \
+	PROPERTY ( _name, _state, _field, const char *, &string_property )
+
+extern size_t property_format ( struct property *prop, char *buf, size_t len,
+				const void *state );
+extern char * property_format_alloc ( struct property *prop,
+				      const void *state );
+extern int property_parse ( struct property *prop, const char *string,
+			    void *state );
+
+#endif /* _UNIPORT_PROPERTY_H */
